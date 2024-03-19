e0e3bf6a5c60b7731342290622fa43aa0351f243
farushed <farushed@tcd.ie>
2024-03-19 17:14:00 +0000

    Add webpack-dev-server
    Allows loading userscript from URL for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
 const path = require('path');
 const { UserscriptPlugin } = require('webpack-userscript');
 
+const dev = process.env.NODE_ENV === 'development';
+
+const userscriptHeaders = {
+  name: "Discord Message Encryption",
+  description: "Encrypts messages before sending, decrypts received messages",
+  version: "0.1",
+  author: "Daniel Farushev",
+  match: "https://discord.com/*",
+  grant: "none",
+}
+
 module.exports = {
+  mode: dev ? 'development' : 'production',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    client: {
+      overlay: {errors: true, warnings: false, runtimeErrors: false},
+    }
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -10,14 +29,12 @@ module.exports = {
   },
   plugins: [
     new UserscriptPlugin({
-      headers: {
-        name: "Discord Message Encryption",
-        description: "Encrypts messages before sending, decrypts received messages",
-        version: "0.1",
-        author: "Daniel Farushev",
-        match: "https://discord.com/*",
-        grant: "none",
-      }
-    })
+      headers: dev
+        ? {
+          ...userscriptHeaders,
+          version: `${userscriptHeaders.version}-build.[buildTime]`,
+        }
+        : userscriptHeaders
+    }),
   ],
 };
